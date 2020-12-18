15ba08e1d7e9bfc66685b52b1a0139ddb69c6d8c
Levi <levi.ciarrocchi@gmail.com>
2020-12-18 14:25:12 -0300

    Exercises completed

--- Basics Js/index.js
+++ Basics Js/index.js
@@ -65,7 +65,7 @@ var myArr = [20, 7, 3, 2, 1, 8, 9];
 function media(arr) {
      var soma = 0;
      for (let num of arr) {
-           soma += arr[num];
+           soma += num;
      }
 
      var media = soma / arr.length
@@ -78,29 +78,168 @@ function media(arr) {
 // Resolution exercise 5
 
 function evenOrOdd(num) {
-
     return num % 2 == 0 ? "É par" : "É ímpar";
-
 }
 
 // -------------------------------------------------------------------------------------//
 
 // Resolution exercise 6
 
+function pi() {
+    for (var i = 0; i < 101; i++) {
+        if (i % 4 == 0) {
+            console.log("pi")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+pi()
+
 // -------------------------------------------------------------------------------------//
 
 // Resolution exercise 7
 
+const myObject = {
+    name: "Nicolas",
+    hair: "Brown",
+    height: "1,83"
+}
+
+function properties(object) {
+    console.log(Object.keys(object))
+}
+
+// or
+
+function properties2(object) {
+    for (let propertie in object) {
+        console.log(propertie)
+    }
+}
+
+properties(myObject)
+
 // -------------------------------------------------------------------------------------//
 
 // Resolution exercise 8
+var meses = ['Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro']
+    
+
+function date(dia) {
+    const day = dia.getDate();
+    const month = meses[dia.getMonth()];
+    const year = dia.getFullYear();
+
+    return console.log(day, month, year)
+}
+
+date(new Date(Date.now()))
+
+// or
+
+function data(d){
+   return {
+        dia:d.getDate(),
+        mes: meses[d.getMonth()],
+        ano: d.getFullYear()
+    }
+}
+
+console.log(data(new Date(Date.now())))
 
 // -------------------------------------------------------------------------------------//
 
 // Resolution exercise 9
 
+var arrayOfObjects = [
+    {
+        height: 182,
+        age: 20
+    },
+
+    {
+        height: 162,
+        age: 50
+    },
+
+    {
+        height: 190,
+        age: 30
+    },
+
+    {
+        height: 210,
+        age: 100
+    },
+
+    {
+        height: 150,
+        age: 2
+    }
+];
+
+function analysis(arr) {
+    var maiorAltura = 0;
+    var menorAltura = 999;
+    var idades = []
+
+    for (var object of arr) {
+
+        if (object.height > maiorAltura) {
+            maiorAltura = object.height
+
+        } else if (object.height < menorAltura) {
+            menorAltura = object.height
+        }
+        
+        idades.push(object.age)
+    }
+
+    function mediaArray(arr) {
+        var soma = 0;
+        for (let num of arr) {
+              soma += num;
+        }
+   
+        var media = soma / arr.length
+   
+        return media
+   }
+
+    console.log("A maior altura é: " + maiorAltura)
+    console.log("A menor altura é: " +menorAltura)
+    console.log("A média entre as idades é: " + mediaArray(idades))
+
+    
+}
+
 // -------------------------------------------------------------------------------------//
 
 // Resolution exercise 10
+
+function spikes() {
+    for (var i = 1; i < 6; i++) {
+        let string = ""
+        for (var j = 1; j <= i; j++){
+            string += "* "
+        }
+        console.log(string)
+    }
+}
+
+spikes()
 
 // -------------------------------------------------------------------------------------//
