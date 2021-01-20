986bcdcae2374e17b1cc9f770cb13946e8cc9b5d
Levi <levi.ciarrocchi@gmail.com>
2021-01-20 19:31:50 -0300

    Image Editor

--- Nodejs/ImageEditor/app.js
+++ Nodejs/ImageEditor/app.js
@@ -1,4 +1,6 @@
 const sharp = require('sharp');
+const compress_images = require('compress-images');
+const fs = require('fs');
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
@@ -12,13 +14,35 @@ function resize(inputPath, outputPath, width) {
             console.log(err);
         } else {
             console.log('Imagem redimensionada com sucesso');
+            compress(outputPath, "./compressed/");
         }
     });
     
 }
 
 function compress(pathInput, outputPath) {
-    
+    compress_images(pathInput, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+
+            fs.unlink(pathInput, (err) => {
+                if (err) {
+                    console.log(err)    
+                } else {
+                    console.log(pathInput, " Apagado")
+                }
+            })
+        }
+);
 }
 
 resize(path, './temp/output_resize.jpg', width);
